030893bddffc8dfbbbe5fd085cf19aae4615bb06
nyagah-max <kelvinnyagah045@gmail.com>
2023-11-09 11:43:38 +0300

    welcome , accessToken generator and stkpush files

--- stkPushApi.php
+++ stkPushApi.php
@@ -1,42 +1,56 @@
 <?php
 
+//For us to be able to access the authorization token
 include("accessTokenGeneration.php");
+
+//setting the correct time zone so that the timestamp is precise and up to date
 date_default_timezone_set("Africa/Nairobi");
-$processRequestUrl = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
-$callbackUrl = "http://nyagahtest.000webhostapp.com/daraja/daraja.php";
-$passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-$bussinessShortCode = "174379";
 $timeStamp = date('YmdHis');
+
+
+//initiating the curl now
+$ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer 4PR9VnA1BrBJVeLhALlAk8KC3lyZ',
+    'Content-Type: application/json'
+]);
+curl_setopt($ch, CURLOPT_POST, 1);
+$bussinessShortCode = 174379;
+$timeStamp = date('YmdHis');
+
+//passkey goten from the apis section, authorization then your app's test credentials
+$passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+
+
+//encrypting the shrortcode, passkey and timestamp to generate password
 $password = base64_encode($bussinessShortCode . $passkey . $timeStamp);
-$phone = '254740467735';
-$money = '1';
-$partyA = $phone;
-$partyB = '254708374149';
-$accountReference = 'Vins Labs';
-$transactionDesc = 'stkPush test';
-$amount = $money;
-$stkpushheader = ['content-Type:applicatio/json', 'Authorixation:Bearer ' . $accessToken];
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $initiate_url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader);
+
+//building the post payload data
 $curl_post_data = array(
-    'bussinessShortCode' => $bussinessShortCode,
-    'Password' => $password,
-    'Timestamp' => $timeStamp,
-    'TransactionType' => 'CustomerpaybillOnline',
-    'Amount' => $amount,
-    'Partya' => $partyA,
-    'PartyB' => $bussinessShortCode,
-    'PhoneNumber' => $partyA,
-    'CallbackUrl' => $callbackUrl,
-    'AccountReference' => $accountReference,
-    'TransactionDesc' => $transactionDesc
+    'BusinessShortCode' => 174379,  // app's credentials'. Because it's not a live app but a sandboxed one
+    "Password" => base64_encode($bussinessShortCode . $passkey . $timeStamp),
+    "Timestamp" => $timeStamp,
+    "TransactionType" => "CustomerPayBillOnline",
+    "Amount" => 1,
+    "PartyA" => 254740467735,
+    "PartyB" => 174379,
+    "PhoneNumber" => 254740467735,// your phone number or rather the phone number to get the stk payment request on
+    "CallBackURL" => "http://nyagahtest.000webhostapp.com/daraja/daraja.php",
+    "AccountReference" => "CompanyXLTD",
+    "TransactionDesc" => "Payment of X"
 );
 
+//preparing the post payload data
 $dataString = json_encode($curl_post_data);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $dataString);
 
-$curl_response = curl_exec($curl);
+//using the post payload now
+curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+// executing and displaying the response and we get an stk push on our phone(PartyA)
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
+
 
